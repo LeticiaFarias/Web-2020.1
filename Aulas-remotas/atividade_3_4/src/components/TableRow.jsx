109aec5748f227a842dabf1b355c20312134c4c5
leticia <leticia_farias@alu.ufc.br>
2020-08-16 19:51:03 -0300

    Atividade 3 e 4

---
+++ Aulas-remotas/atividade_3_4/src/components/TableRow.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default class TableRow extends Component {
+   constructor(props) {
+      super(props);
+      this.apagar = this.apagar.bind(this);
+   }
+   apagar() {
+      axios
+         .delete(
+            "http://localhost:3001/disciplinas/" + this.props.disciplina.id
+         )
+         .then((response) => {
+            this.props.deleteElementById(this.props.disciplina.id);
+         })
+         .catch((error) => console.log(error));
+   }
+
+   render() {
+      return (
+         <tr>
+            <td>{this.props.disciplina.id}</td>
+            <td>{this.props.disciplina.nome}</td>
+            <td>{this.props.disciplina.curso}</td>
+            <td>{this.props.disciplina.capacidade}</td>
+            <td style={{ textAlign: "center" }}>
+               <Link
+                  to={"/edit/" + this.props.disciplina.id}
+                  className="btn btn-primary"
+               >
+                  Editar
+               </Link>
+            </td>
+            <td style={{ textAlign: "center" }}>
+               <button onClick={this.apagar} className="btn btn-danger">
+                  Apagar
+               </button>
+            </td>
+         </tr>
+      );
+   }
+}
