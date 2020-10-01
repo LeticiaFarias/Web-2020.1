6be0f156990ff01be26c375e513b4f07eab374eb
leticiafarias <leticia_farias@alu.ufc.br>
2020-10-01 17:50:06 -0300

    🐛 Removendo bug da função Update

--- Aulas-remotas/atividade_8/frontend/src/components/TableRow.jsx
+++ Aulas-remotas/atividade_8/frontend/src/components/TableRow.jsx
@@ -11,10 +11,10 @@ export default class TableRow extends Component {
     axios
       .delete(
         "http://localhost:3003/disciplinas/delete/" +
-          this.props.disciplina.idDisciplina
+          this.props.disciplina._id
       )
       .then((response) => {
-        this.props.deleteElementById(this.props.disciplina.idDisciplina);
+        this.props.deleteElementById(this.props.disciplina._id);
       })
       .catch((error) => console.log(error));
   }
@@ -22,13 +22,13 @@ export default class TableRow extends Component {
   render() {
     return (
       <tr>
-        <td>{this.props.disciplina.idDisciplina}</td>
+       
         <td>{this.props.disciplina.nome}</td>
         <td>{this.props.disciplina.curso}</td>
         <td>{this.props.disciplina.capacidade}</td>
         <td style={{ textAlign: "center" }}>
           <Link
-            to={"/edit/" + this.props.disciplina.idDisciplina}
+            to={"/edit/" + this.props.disciplina._id}
             className="btn btn-primary"
           >
             Editar
