269a999bcc9835e772cd511306cdbe3658857982
leticiafarias <leticia_farias@alu.ufc.br>
2020-10-02 20:54:36 -0300

    ✨ Implementando Edit component

--- Aulas-remotas/atividade_9/crud/src/components/Edit.jsx
+++ Aulas-remotas/atividade_9/crud/src/components/Edit.jsx
@@ -1,98 +1,104 @@
 import React, { Component } from "react";
-import axios from "axios";
 
-export default class Edit extends Component {
-   constructor(props) {
-       super(props);
-      this.state = { nome: "", curso: "", capacidade: "" };
-      this.setNome = this.setNome.bind(this);
-      this.setCurso = this.setCurso.bind(this);
-      this.setCapacidade = this.setCapacidade.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-   }
+import FirebaseContext from "../utils/FirebaseContext";
 
-   setNome(e) {
-      this.setState({ nome: e.target.value });
-   }
-   setCurso(e) {
-      this.setState({ curso: e.target.value });
-   }
-   setCapacidade(e) {
-      this.setState({ capacidade: e.target.value });
-   }
-   componentDidMount() {
-      axios
-         .get("http://localhost:3001/disciplinas/" + this.props.match.params.id)
-         .then((response) => {
-            this.setState({
-               nome: response.data.nome,
-               curso: response.data.curso,
-               capacidade: response.data.capacidade,
-            });
-         })
-         .catch((error) => {
-            console.log(error);
-         });
-   }
-   onSubmit(e) {
-      e.preventDefault();
-      const disciplinaAtualiazada = {
-         nome: this.state.nome,
-         curso: this.state.curso,
-         capacidade: this.state.capacidade,
-      };
-      axios
-         .put(
-            "http://localhost:3001/disciplinas/" + this.props.match.params.id,
-            disciplinaAtualiazada
-         )
-         .then((response) => {
-            this.props.history.push("/list");
-         })
-         .catch((error) => console.log(error));
-   }
+const EditPage = (props) => (
+  <FirebaseContext.Consumer>
+    {(contexto) => <Edit firebase={contexto} id={props.match.params.id} />}
+  </FirebaseContext.Consumer>
+);
 
-   render() {
-      return (
-         <div style={{ marginTop: 10 }}>
-            <h3>Criar Disciplina</h3>
-            <form onSubmit={this.onSubmit}>
-               <div className="form-group">
-                  <label>Nome: </label>
-                  <input
-                     type="text"
-                     className="form-control"
-                     value={this.state.nome}
-                     onChange={this.setNome}
-                  />
-               </div>
-               <div className="form-group">
-                  <label>Curso: </label>
-                  <input
-                     type="text"
-                     className="form-control"
-                     value={this.state.curso}
-                     onChange={this.setCurso}
-                  />
-               </div>
-               <div className="form-group">
-                  <label>Capacidade: </label>
-                  <input
-                     type="text"
-                     className="form-control"
-                     value={this.state.capacidade}
-                     onChange={this.setCapacidade}
-                  />
-               </div>
-               <div className="form-group">
-                  <input
-                     type="submit"
-                     value="Criar"
-                     className="btn btn-primary"
-                  />
-               </div>
-            </form>
-         </div>
-      );
-   }
+class Edit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nome: "", curso: "", capacidade: "" };
+    this.setNome = this.setNome.bind(this);
+    this.setCurso = this.setCurso.bind(this);
+    this.setCapacidade = this.setCapacidade.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.firebase
+      .getFirestore()
+      .collection("disicplinas")
+      .doc(this.props.id)
+      .get()
+      .then((doc) => {
+        this.setState({
+          nome: doc.data().nome,
+          curso: doc.data().curso,
+          capacidade: doc.data().capacidade,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+  setNome(e) {
+    this.setState({ nome: e.target.value });
+  }
+  setCurso(e) {
+    this.setState({ curso: e.target.value });
+  }
+  setCapacidade(e) {
+    this.setState({ capacidade: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.firebase
+      .getFirestore()
+      .collection("disicplinas")
+      .doc(this.props.id)
+      .set({
+        nome: this.setState.nome,
+        curso: this.setState.curso,
+        capacidade: this.setState.capacidade,
+      })
+      .then(() => {
+        console.log("Disciplina atualizada com sucesso!");
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: 10 }}>
+        <h3>Criar Disciplina</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Nome: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.nome}
+              onChange={this.setNome}
+            />
+          </div>
+          <div className="form-group">
+            <label>Curso: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.curso}
+              onChange={this.setCurso}
+            />
+          </div>
+          <div className="form-group">
+            <label>Capacidade: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.capacidade}
+              onChange={this.setCapacidade}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Criar" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default EditPage;
